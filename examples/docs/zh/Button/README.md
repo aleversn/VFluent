7518ee60674d8a072db7e0c083a972bc5fed8659
Alever Lai <759636538@qq.com>
2019-12-16 11:25:36 +0800

    [update] Adding custom styles for button.

--- examples/docs/zh/Button/README.md
+++ examples/docs/zh/Button/README.md
@@ -11,8 +11,20 @@
 
 ### Button With Icon
 ---
-<fv-button icon="CalculatorAddition">Add</fv-button>
+<fv-button icon="CalculatorAddition" theme="dark">Add</fv-button>
 
 ### Button Disabled
 ---
 <fv-button disabled icon="CalculatorAddition">Add</fv-button>
+
+### Propoties
+---
+| 属性(attr)  |             类型(type)             | 必填(required) | 默认值(default) |     说明(statement)     |
+|:-----------:|:----------------------------------:|:--------------:|:---------------:|:-----------------------:|
+|    icon     |              [string]              |       No       |       N/A       |  Icon with Fabric-Icon  |
+| foreground  |              [string]              |       No       |       N/A       |    Button foreground    |
+| background  |              [string]              |       No       |       N/A       |    Button background    |
+| fontWeight  |              [string]              |       No       |     normal      | Button text font weight |
+|  disabled   |               [bool]               |       No       |      false      |     Disabled Button     |
+| borderWidth |              [number]              |       No       |        2        |   Button border width   |
+|    theme    | ['light','dark','custom','system'] |       No       |     system      |      Custom theme       |
--- packages/Button/src/index.vue
+++ packages/Button/src/index.vue
@@ -1,8 +1,8 @@
 <template>
-<div :class="'fv-'+theme+'-button'">
-    <div class="reveal-border" :class="{disabled:(disabled||disabled=='disabled'||disabled=='')}">
-        <button :disabled="disabled" class="button-container">
-            <span class="content-block">
+<div :class="'fv-'+$theme+'-button'">
+    <div class="reveal-border" :class="{disabled:(disabled||disabled=='disabled'||disabled=='')}" :style="styles.revealBorder">
+        <button :disabled="disabled" class="button-container" :style="styles.buttonContainer" @click="onClick">
+            <span class="content-block" :style="styles.contentBlock">
                 <i class="ms-Icon" :class="`ms-Icon--${icon}`" :style="{'margin-right': icon != '' ? '5px': ''}"></i>
                 <slot>
                     Button
@@ -23,39 +23,84 @@ export default {
         icon: {
             default: ''
         },
+        theme: {
+            default: 'system'
+        },
+        foreground: {
+            default: ''
+        },
+        background: {
+            default: ''
+        },
+        fontWeight: {
+            default: 'normal'
+        },
         disabled: {
             default: false
+        },
+        borderWidth: {
+            default: 2
         }
     },
     data () {
         return {
-            
+            styles: {
+                revealBorder: {
+                    padding: '2px'
+                },
+                buttonContainer: {
+                    background: ''
+                },
+                contentBlock: {
+                    color: '',
+                    fontWeight: 'normal'
+                }
+            }
+        }
+    },
+    watch: {
+        theme (val) {
+            this.FRInit();
+        },
+        borderWidth (val) {
+            this.stylesInit();
+        },
+        foreground (val) {
+            this.stylesInit();
+        },
+        background (val) {
+            this.stylesInit();
+        },
+        fontWeight (val) {
+            this.stylesInit();
         }
     },
     computed: {
-        theme () {
-            return this.$fvGlobal.state.theme;
+        $theme () {
+            if(this.theme == 'system')
+                return this.$fvGlobal.state.theme;
+            return this.theme;
         },
         borderLightColor () {
-            if(this.theme == 'light') {
+            if(this.$theme == 'light') {
                 return 'rgba(121, 119, 117, 0.6)';
             }
-            if(this.theme == 'dark' || this.theme == 'custom') {
+            if(this.$theme == 'dark' || this.$theme == 'custom') {
                 return 'rgba(255, 255, 255, 0.6)';
             }
         },
         backgroundLightColor () {
-            if(this.theme == 'light') {
+            if(this.$theme == 'light') {
                 return 'rgba(121, 119, 117, 0.3)';
             }
-            if(this.theme == 'dark' || this.theme == 'custom') {
+            if(this.$theme == 'dark' || this.$theme == 'custom') {
                 return 'rgba(255, 255, 255, 0.3)';
             }
         }
     },
     mounted () {
-        this.$fvGlobal.commit('changeTheme','dark')
         this.FRInit();
+        this.stylesInit();
     },
     methods: {
         FRInit () {
@@ -65,12 +110,21 @@ export default {
                 gradientSize: 80,
                 isContainer: true,
                 children: {
-                    borderSelector: ".reveal-border",
-                    elementSelector: ".button-container",
+                    borderSelector: `.fv-${this.$theme}-button .reveal-border`,
+                    elementSelector: `.fv-${this.$theme}-button .button-container`,
                     lightColor: this.backgroundLightColor,
                     gradientSize: 120
                 }
             });
+        },
+        stylesInit () {
+            this.styles.revealBorder.padding = `${this.borderWidth}px`;
+            this.styles.contentBlock.color = this.foreground;
+            this.styles.buttonContainer.background = this.background;
+            this.styles.contentBlock.fontWeight = this.fontWeight;
+        },
+        onClick (event) {
+            this.$emit('click', event);
         }
     }
 };
