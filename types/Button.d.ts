09d2d6ddc9c428c0c706c01a65d849b98e9fa6e7
minskiter <minskiter@qq.com>
2019-12-17 22:28:16 +0800

    [update]@types helper,better work in vscode

---
+++ types/Button.d.ts
@@ -0,0 +1,20 @@
+import {FluentVueComponent,FluentVueTheme} from './components'
+
+/**
+ * Fluent Vue Button Component
+ */
+export declare class FvButton extends FluentVueComponent{
+  icon : string
+
+  foreground:string
+
+  background:string
+
+  fontWeight:string
+
+  disabled:boolean
+
+  borderWidth:number
+
+  theme:FluentVueTheme
+}
---
+++ types/components.d.ts
@@ -0,0 +1,15 @@
+  import Vue from 'vue'
+
+/** declare FluentVue Component */
+export declare class FluentVueComponent extends Vue {
+  /** Install component into Vue */
+  static install (vue: typeof Vue): void
+}
+
+/** Component size definition for button, input, etc */
+export type FluentVueComponentSize =  'l' | 'm'  | 's' | 'xs' 
+
+/** Horizontal alignment */
+export type FluentVueHorizontalAlignment = 'left' | 'center' | 'right'
+
+export type FluentVueTheme = 'light' | 'dark' | 'system' | 'custom'
