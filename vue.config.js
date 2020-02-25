c5fca27720bb26b7ab0a6b4a92a9c84d9545090c
minskiter <minskiter@qq.com>
2020-02-25 16:10:11 +0800

    [fix] fix vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+module.exports = {
+  pages: {
+    index: {
+      entry: "examples/index.js"
+    }
+  },
+  css: {
+    // Separate StyleSheet
+    extract: true,
+    modules: false,
+    sourceMap: false
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("packages/"));
+    // config.module
+    //   .rule("js")
+    //   .include.add("/packages")
+    //   .end()
+    //   .use("babel-loader")
+    //   .loader("babel-loader")
+    //   .tap(options => {
+    //     return options;
+    //   });
+  },
+  lintOnSave: false
+};
