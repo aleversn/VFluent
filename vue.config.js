23676a0b4ad5c9d3de5e1bb1cc748bd16322952c
minskiter <minskiter@qq.com>
2020-03-03 11:09:33 +0800

    [fix] missing css stylesheet

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
   css: {
     // Separate StyleSheet
     extract: true,
-    requireModuleExtension:false,
+    requireModuleExtension:true,
     sourceMap: false
   },
   productionSourceMap:false,
