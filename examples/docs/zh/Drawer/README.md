71a8b02a10aa4a59cb665608eccf8c3b40d1110c
minskiter <minskiter@qq.com>
2022-03-15 15:43:53 +0800

    [update] Drawer.vue - add option appendBody for IOS compatibility

--- examples/docs/zh/Drawer/README.md
+++ examples/docs/zh/Drawer/README.md
@@ -15,13 +15,10 @@
     },
     methods:{
       toggle(){
-        this.visible^=true;
+        this.visible=!this.visible;
       }
     }, 
     computed:{
-      $theme(){
-        return !this.theme?'light':'dark';
-      },
       divStyle(){
         if (this.$theme=='light'){
           return {
@@ -43,34 +40,32 @@
 
 ### Drawer
 
-<div :style="divStyle">
-THEME:<fv-toggle-switch v-model="theme" :on="$theme" :off="$theme" :theme="$theme"/>
-</div>
 
-<fv-button @click="toggle" :theme="$theme">Show/Hide</fv-button>
+<fv-button @click="toggle" >Show/Hide</fv-button>
 
-<fv-drawer :visible.sync="visible" :theme="$theme">
+<fv-drawer :visible.sync="visible" position="top" appendBody>
 </fv-drawer>
 
 ``` vue
-<fv-drawer :visible.sync="visible" :theme="$theme">
+<fv-drawer :visible.sync="visible" appendBody>
 </fv-drawer>
 ```
 
 ### Propoties
 ---
-| 属性(attr)  |             类型(type)             | 必填(required) | 默认值(default) |     说明(statement)     |
-|:-----------:|:----------------------------------:|:--------------:|:---------------:|:-----------------------:|
-| theme | ['system','dark','light','custom'] | No | 'system' | 主题色 |
-| visible | [boolean] | No | false | 是否可视 |
-| length | [number|string] | No | 300 | 抽屉长度 |
-| focusTrap | [boolen] | No | false | 是否聚焦 |
-| disabled | [boolean] | No | false | 是否禁用选项框 |
-| position | ['bottom','left','top','right'] | No | 'bottom' | 位置 |
+| 属性(attr) |             类型(type)             | 必填(required) | 默认值(default) |      说明(statement)      |
+| :--------: | :--------------------------------: | :------------: | :-------------: | :-----------------------: |
+|   theme    | ['system','dark','light','custom'] |       No       |    'system'     |          主题色           |
+|  visible   |             [boolean]              |       No       |      false      |         是否可视          |
+|   length   |          [number, string]          |       No       |       300       |         抽屉长度          |
+| focusTrap  |              [boolen]              |       No       |      false      |         是否聚焦          |
+|  disabled  |             [boolean]              |       No       |      false      |      是否禁用选项框       |
+|  position  |  ['bottom','left','top','right']   |       No       |    'bottom'     |           位置            |
+| appendBody |             [boolean]              |       No       |      false      | 是否添加到body（IOS兼容） |
 
 ### Events
 ---
 | 事件名(Name) | 参数类型(args) | 说明(statement) |
-|:------------:|:--------------:|:---------------:|
+| :----------: | :------------: | :-------------: |
 |              |                |                 |
   
--- packages/Drawer/src/index.vue
+++ packages/Drawer/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="['fv-'+$theme+'-drawer']" :style="style.drawer">
+    <div ref="drawer" :class="['fv-' + $theme + '-drawer']" :style="style.drawer">
         <slot></slot>
     </div>
 </template>
@@ -10,143 +10,180 @@ export default {
     props: {
         theme: {
             type: String,
-            default: "system"
+            default: 'system',
         },
-        position:{
-            type:String,
-            default:"bottom"
+        position: {
+            type: String,
+            default: 'bottom',
         },
-        length:{
-            default: 300
+        length: {
+            type: [Number, String],
+            default: 300,
         },
-        visible:{
+        visible: {},
+        zIndex: {
+            type: Number,
+            default: 9999,
         },
-        zIndex:{
-            type:Number,
-            default:9999
+        focusTrap: {
+            type: Boolean,
+            default: false,
         },
-        focusTrap:{
-            type:Boolean,
-            default:false,
-        }
+        appendBody: {
+            type: Boolean,
+            default: false,
+        },
     },
-    data () {
+    data() {
         return {
-            lock:false,
-            style:{
-                drawer:{
-                    zIndex:this.zIndex
-                }
+            lock: false,
+            style: {
+                drawer: {
+                    zIndex: this.zIndex,
+                },
             },
-            show:{
-                drawer:this.visible==undefined?false:this.visible,
+            show: {
+                drawer: this.visible == undefined ? false : this.visible,
             },
-            window:{
-                click:(evt)=>{
+            window: {
+                click: (evt) => {
                     if (!this.computeVisible || this.lock) return;
                     if (this.focusTrap) return;
                     let el = evt.target;
                     let _self = false;
-                    while (el){
-                        if (el==this.$el){
+                    while (el) {
+                        if (el == this.$el) {
                             _self = true;
                         }
-                        if (el.parentNode)
-                            el=el.parentNode;
+                        if (el.parentNode) el = el.parentNode;
                         else break;
                     }
-                    if (!_self) this.computeVisible=false;       
-                }
-            }
-        }
+                    if (!_self) this.computeVisible = false;
+                },
+            },
+            parentBox: null,
+            parent: {
+                scroll: () => {
+                    // to follow scroll
+                    let top = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+                    let left = document.documentElement.scrollLeft || window.pageXOffset || document.body.scrollLeft;
+                    this.parentBox.style.top = `${top}px`;
+                    this.parentBox.style.left = `${left}px`;
+                },
+            },
+        };
     },
     computed: {
-        $theme () {
-            if (this.theme=='system')
-                return this.$fvGlobal.state.theme;
+        $theme() {
+            if (this.theme == 'system') return this.$fvGlobal.state.theme;
             return this.theme;
         },
-        computeVisible:{
-            get(){
-                return this.visible==undefined?this.show.drawer:this.visible;
+        computeVisible: {
+            get() {
+                return this.visible == undefined ? this.show.drawer : this.visible;
             },
-            set(val){
+            set(val) {
                 this.show.drawer = val;
-                this.$emit("update:visible",val);
-            }
-        }
+                this.$emit('update:visible', val);
+            },
+        },
     },
-    watch:{
-        computeVisible(){
-            this.lock=true;
+    watch: {
+        computeVisible() {
+            this.lock = true;
             this.setStyle();
             // Avoid Dumplite Touch
-            setTimeout(()=>{
-                this.lock=false;
-            },50);
+            setTimeout(() => {
+                this.lock = false;
+            }, 50);
         },
     },
-    mounted(){
-        this.setStyle();
+    mounted() {
         this.init();
+        this.setStyle();
     },
-    methods:{
-        init(){
-            for (let key in this.window){
-                window.addEventListener(key,this.window[key]);
+    beforeDestroy() {
+        for (let key in this.window) {
+            window.removeEventListener(key, this.window);
+        }
+        if (this.parentBox != null) {
+            this.parentBox.remove();
+            window.removeEventListener('scroll', this.parent.scroll);
+        }
+    },
+    methods: {
+        init() {
+            for (let key in this.window) {
+                window.addEventListener(key, this.window[key]);
+            }
+            // For compatibility with IOS
+            if (this.appendBody) {
+                // change position style: absolute
+                this.parentBox = document.createElement('div');
+                document.body.append(this.parentBox);
+                this.$refs.drawer.remove();
+                this.parentBox.append(this.$refs.drawer);
+                this.$refs.drawer.style.position = 'absolute';
+                this.$refs.drawer.style.visibility = 'visible';
+
+                this.parentBox.style.top = '0';
+                this.parentBox.style.left = '0';
+                this.parentBox.style.position = 'absolute';
+                this.parentBox.style.width = '100%';
+                this.parentBox.style.height = '100%';
+                this.parentBox.style.background = 'red';
+                this.parentBox.style.zIndex = 9999;
+                this.parentBox.style.overflow = 'hidden';
+                this.parentBox.style.visibility = 'hidden';
+                window.addEventListener('scroll', this.parent.scroll);
             }
         },
-        setStyle(){
+        setStyle() {
             let length = this.length;
-            if (typeof(this.length)=="number"){
-                length+='px';
+            if (typeof this.length == 'number') {
+                length += 'px';
             }
-            if (this.position=="bottom"){
-                this.style.drawer={
-                    left:"0",
-                    bottom:"0",
-                    height:length,
-                    width:"100%",
-                    zIndex:this.zIndex,
-                    transform:` ${this.computeVisible?"translateY(0%)":"translateY(100%)"}`
-                }
-            }else if (this.position=="top"){
-                this.style.drawer={
-                    left:"0",
-                    top:"0",
-                    height:length,
-                    width:"100%",
-                    zIndex:this.zIndex,
-                    transform:`${this.computeVisible?"translateY(0%)":"translateY(-100%)"}`
-                }
-            }else if (this.position=="left"){
-                this.style.drawer={
-                    left:"0",
-                    top:"0",
-                    width:length,
-                    height:"100%",
-                    zIndex:this.zIndex,
-                    transform:`${this.computeVisible?"translateX(0%)":"translateX(-100%)"}`
-                }
-            }else{
-                this.style.drawer={
-                    right:"0",
-                    top:"0",
-                    width:length,
-                    height:"100%",
-                    zIndex:this.zIndex,
-                    transform:`${this.computeVisible?"translateX(0%)":"translateX(100%)"};`
-                }
+            if (this.position == 'bottom') {
+                this.style.drawer = {
+                    left: '0',
+                    bottom: '0',
+                    height: length,
+                    width: '100%',
+                    zIndex: this.zIndex,
+                    transform: ` ${this.computeVisible ? 'translateY(0%)' : 'translateY(110%)'}`,
+                };
+            } else if (this.position == 'top') {
+                this.style.drawer = {
+                    left: '0',
+                    top: '0',
+                    height: length,
+                    width: '100%',
+                    zIndex: this.zIndex,
+                    transform: `${this.computeVisible ? 'translateY(0%)' : 'translateY(-110%)'}`,
+                };
+            } else if (this.position == 'left') {
+                this.style.drawer = {
+                    left: '0',
+                    top: '0',
+                    width: length,
+                    height: '100%',
+                    zIndex: this.zIndex,
+                    transform: `${this.computeVisible ? 'translateX(0%)' : 'translateX(-110%)'}`,
+                };
+            } else {
+                this.style.drawer = {
+                    right: '0',
+                    top: '0',
+                    width: length,
+                    height: '100%',
+                    zIndex: this.zIndex,
+                    transform: `${this.computeVisible ? 'translateX(0%)' : 'translateX(110%)'}`,
+                };
             }
         },
-        close(){
-            this.computeVisible=false;
-        }
+        close() {
+            this.computeVisible = false;
+        },
     },
-    beforeDestroy(){
-        for (let key in this.window){
-            window.removeEventListener(key,this.window);
-        }
-    }
-}
+};
 </script>
