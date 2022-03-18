2f681d0d4a1ca64c4cfccbad75cd827ff4486554
Alever <759636538@qq.com>
2022-03-18 16:31:23 +0800

    [update]

--- packages/Breadcrumb/src/index.vue
+++ packages/Breadcrumb/src/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="['fv-'+$theme+'-Breadcrumb', { readOnly: readOnly, 'editor-mode': mode === 'editor', disabled: isDisabled }]" :style="{'border-radius': `${borderRadius}px`, 'border-color': mode === 'editor' ? borderColor : '', 'outline-color': mode === 'editor' ? borderColor : ''}">
         <div class="fv-bc-route-list" ref="main" @click="editorMode">
-            <div v-show="showRoot" class="fv-bc-route-item">
+            <div v-show="showRoot" class="fv-bc-route-item" @click="routeClick">
                 <slot name="root">
                     <i class="fv-bc-separator-content ms-Icon" :class="[`ms-Icon--${rootIcon}`]"></i>
                     <i class="fv-bc-separator-icon ms-Icon" :class="[separatorIcon ? `ms-Icon--${separator}` : '']">{{separatorIcon ? '' : separator}}</i>
